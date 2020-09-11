b0fbf216a7c6c367c867ca498398c893fcd8d772
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-09-11 10:47:59 -0400

    added new folder for TS files

---
+++ Typescript/ts_app.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+var $ = require("jquery");
+$(document).ready(function () {
+    console.log('I got parcel-bundler to work!');
+    console.log('I made a change and recompiled');
+});
+var cards_1 = require("./cards");
+var chooseRandomImg = function (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+var createCard = function (fn) {
+    var srcUrl = chooseRandomImg(cards_1["default"]);
+    var $imgForCard = $("<img src=" + srcUrl + " alt='card image'>");
+    var $newCard = $('.container').append($imgForCard);
+    return $newCard;
+};
+// const $card = $('<div>');
+var $addBtn = $('.addCard');
+$addBtn.click(function () {
+    createCard(chooseRandomImg(cards_1["default"]));
+});
